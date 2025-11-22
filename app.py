b2df83dd73795c72c931e5cfecc426a6a6972902
Rohit-8626 <rohitvastani8626@gmail.com>
2025-11-22 21:21:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import re
+import joblib
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+model = joblib.load('log_reg_model.pkl')
+tfidf = joblib.load('tfidf_vectorizer.pkl')
+
+stop_words = set(stopwords.words('english'))
+lammetizer = WordNetLemmatizer()
+
+def clean_text(text):
+
+    text = text.lower()
+    text = re.sub(r'[^a-z\s]', ' ' , text)
+    words = word_tokenize(text)
+    words = [lammetizer.lemmatize(word) for word in words if word not in stop_words]
+    return " ".join(words)
+
+st.title('Amazon Review Sentiment Analyzer')
+
+review = st.text_area('Enter Product Review : ')
+
+if st.button("Analyze"):
+    if not review.strip():
+        st.warning("Please first enter the review")
+    else:
+        cleaned = clean_text(review)
+        vec = tfidf.transform([cleaned])
+        pred = model.predict(vec)
+        st.subheader("Sentiment : " + ("Positive 😊" if pred == 1 else "Negative 😡"))
