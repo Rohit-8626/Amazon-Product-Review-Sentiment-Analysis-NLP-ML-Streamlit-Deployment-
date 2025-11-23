2299e6845684ae5cff972427873149557b7da1a0
Rohit Vastani <rohitvastani8626@gmail.com>
2025-11-23 09:11:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,7 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
-nltk.download('stopwords', quiet=True)
-nltk.download('wordnet', quiet=True)
-nltk.download('punkt', quiet=True)
-
+stop_words = set(stopwords.words('english'))
 
 model = joblib.load('log_reg_model.pkl')
 tfidf = joblib.load('tfidf_vectorizer.pkl')
@@ -21,7 +18,7 @@ def clean_text(text):
 
     text = text.lower()
     text = re.sub(r'[^a-z\s]', ' ' , text)
-    words = word_tokenize(text)
+    words = text.split()
     words = [lammetizer.lemmatize(word) for word in words if word not in stop_words]
     return " ".join(words)
 
@@ -38,6 +35,7 @@ if st.button("Analyze"):
         pred = model.predict(vec)
 
         st.subheader("Sentiment : " + ("Positive 😊" if pred == 1 else "Negative 😡"))
+
 
 
 
